e271abdd4cb47ebd5effcc520cba6bca232aebd8
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-06-15 22:57:42 +0200

    feat(nixos): cap boot generations and enable scheduled nix gc

--- hosts/common/nix-settings.nix
+++ hosts/common/nix-settings.nix
@@ -3,6 +3,13 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     trusted-users = [ "root" "@wheel" ];
+    auto-optimise-store = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
   };
 
   programs.ssh.extraConfig = ''
