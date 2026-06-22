1c67e9eb0bb91b2ab87d2c06ce1faf2f376c7c14
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-06-23 01:01:29 +0200

    refactor(router): gate kiosk desktop behind services.kioskConsole flag

--- modules/router/display.nix
+++ modules/router/display.nix
@@ -1,12 +1,19 @@
-{ pkgs, ... }:
 {
-  hardware.graphics.enable = true;
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  config = lib.mkIf config.services.kioskConsole.enable {
+    hardware.graphics.enable = true;
 
-  fonts = {
-    enableDefaultPackages = true;
-    packages = with pkgs; [
-      dejavu_fonts
-      noto-fonts
-    ];
+    fonts = {
+      enableDefaultPackages = true;
+      packages = with pkgs; [
+        dejavu_fonts
+        noto-fonts
+      ];
+    };
   };
 }
