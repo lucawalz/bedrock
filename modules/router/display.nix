12be24b90cc478c2f7c1956a11e4f09aa1ebf9e4
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-06-24 02:11:04 +0200

    fix(router): drive bar panel at native 400x1280 portrait via initrd edid

--- modules/router/display.nix
+++ modules/router/display.nix
@@ -5,21 +5,26 @@
   ...
 }:
 let
-  wisecocoEdid = pkgs.runCommand "wisecoco-1280x400-edid" { } ''
-    install -Dm444 ${./wisecoco-1280x400.bin} "$out/lib/firmware/edid/wisecoco-1280x400.bin"
+  edidName = "wisecoco-400x1280.bin";
+  edidFirmware = pkgs.runCommand "wisecoco-edid-firmware" { } ''
+    install -Dm444 ${./wisecoco-400x1280.bin} "$out/lib/firmware/edid/${edidName}"
+  '';
+  edidInitrd = pkgs.runCommand "wisecoco-edid-initrd" { } ''
+    mkdir -p root/lib/firmware/edid
+    cp ${./wisecoco-400x1280.bin} root/lib/firmware/edid/${edidName}
+    ( cd root && find . -print0 | sort -z | ${pkgs.cpio}/bin/cpio -o -H newc --null ) > $out
   '';
 in
 {
   config = lib.mkIf config.services.kioskConsole.enable {
     hardware.graphics.enable = true;
 
-    hardware.display = {
-      edid.packages = [ wisecocoEdid ];
-      outputs."HDMI-A-1" = {
-        edid = "wisecoco-1280x400.bin";
-        mode = "e";
-      };
-    };
+    hardware.firmware = [ edidFirmware ];
+    boot.initrd.prepend = [ "${edidInitrd}" ];
+    boot.kernelParams = [
+      "video=HDMI-A-1:e"
+      "drm.edid_firmware=HDMI-A-1:edid/${edidName}"
+    ];
 
     fonts = {
       enableDefaultPackages = true;
