754b68e06ef0490724d3a090a13567bfe71a532a
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-06-24 02:30:05 +0200

    fix(router): drop initrd edid that broke boot, restore 1920x1080 kiosk

--- modules/router/display.nix
+++ modules/router/display.nix
@@ -4,27 +4,9 @@
   config,
   ...
 }:
-let
-  edidName = "wisecoco-400x1280.bin";
-  edidFirmware = pkgs.runCommand "wisecoco-edid-firmware" { } ''
-    install -Dm444 ${./wisecoco-400x1280.bin} "$out/lib/firmware/edid/${edidName}"
-  '';
-  edidInitrd = pkgs.runCommand "wisecoco-edid-initrd" { } ''
-    mkdir -p root/lib/firmware/edid
-    cp ${./wisecoco-400x1280.bin} root/lib/firmware/edid/${edidName}
-    ( cd root && find . -print0 | sort -z | ${pkgs.cpio}/bin/cpio -o -H newc --null ) > $out
-  '';
-in
 {
   config = lib.mkIf config.services.kioskConsole.enable {
     hardware.graphics.enable = true;
-
-    hardware.firmware = [ edidFirmware ];
-    boot.initrd.prepend = [ "${edidInitrd}" ];
-    boot.kernelParams = [
-      "video=HDMI-A-1:e"
-      "drm.edid_firmware=HDMI-A-1:edid/${edidName}"
-    ];
 
     fonts = {
       enableDefaultPackages = true;
