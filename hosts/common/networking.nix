28669a17174339418e63a27d1b66cdebf47a7b68
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-09-06 18:46:06 +0200

    feat(networking): give the nodes static addresses and routes

--- hosts/common/networking.nix
+++ hosts/common/networking.nix
@@ -1,8 +1,35 @@
-{ inventory, ... }:
+{
+  inventory,
+  meta,
+  lib,
+  ...
+}:
+let
+  self = inventory.nodes.${meta.hostname};
+  prefixLength = lib.last (lib.splitString "/" inventory.subnet);
+in
 {
   networking = {
-    networkmanager.enable = true;
-    hosts.${inventory.nodes.master} = [ "master" ];
+    networkmanager = {
+      enable = true;
+      settings.main.no-auto-default = "*";
+      ensureProfiles.profiles.static-node = {
+        connection = {
+          id = "static-node";
+          type = "ethernet";
+          autoconnect = true;
+          autoconnect-priority = 10;
+        };
+        ethernet.mac-address = self.mac;
+        ipv4 = {
+          method = "manual";
+          addresses = "${self.address}/${prefixLength}";
+          inherit (inventory) gateway;
+          dns = inventory.gateway;
+        };
+      };
+    };
+    hosts.${inventory.nodes.master.address} = [ "master" ];
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 ];
